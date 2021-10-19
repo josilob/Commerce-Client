ccd2249c0d59d3144254816232e5199e9ac08d2f
Bojan J <bjosilo@gmail.com>
2021-10-19 17:14:26 -0400

    slider and image added

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,6 +7,14 @@ export const Slider = () => {
 			<Arrow direction='left'>
 				<ChevronLeft />
 			</Arrow>
+			<Wrapper>
+				<Slide>
+					<ImageContainer>
+						<Image src='https://www.designfreelogoonline.com/wp-content/uploads/2020/07/00381-shopping-01.png' />
+					</ImageContainer>
+					<InfoContainer></InfoContainer>
+				</Slide>
+			</Wrapper>
 			<Arrow direction='right'>
 				<ChevronRight />
 			</Arrow>
@@ -24,17 +32,51 @@ const Container = styled.div`
 `;
 
 const Arrow = styled.div`
-	width: 50px;
-	height: 50px;
-	background-color: #fff7f7;
-	border-radius: 50%;
+	& {
+		width: 50px;
+		height: 50px;
+		background-color: #fff7f7;
+		border-radius: 50%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: ${(props) => props.direction === 'left' && '10px'};
+		right: ${(props) => props.direction === 'right' && '10px'};
+		margin: auto;
+		cursor: pointer;
+		opacity: 0.5;
+		transition: opacity 0.25s linear;
+	}
+
+	&:hover {
+		opacity: 1;
+	}
+`;
+
+const Wrapper = styled.div`
+	height: 100%;
+`;
+
+const Slide = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: ${(props) => props.direction === 'left' && '10px'};
-	right: ${(props) => props.direction === 'right' && '10px'};
-	margin: auto;
+	width: 100vw;
+	height: 100vh;
+`;
+const ImageContainer = styled.div`
+	height: 100%;
+	flex: 1;
+`;
+
+const InfoContainer = styled.div`
+	flex: 1;
+`;
+
+const Image = styled.img`
+	height: 80%;
+	max-height: 600px;
 `;
