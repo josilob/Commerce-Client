6c2e092c14f168b8b1819b2ccfd7faf4173721e9
Bojan J <bjosilo@gmail.com>
2021-10-21 19:48:25 -0400

    single product layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Home } from './components/Home/Home';
+import { Product } from './components/Product/Product';
 import { ProductList } from './components/Products/ProductList';
 
 export const App = () => {
 	// return <Home />;
-	return <ProductList />;
+	// return <ProductList />;
+	return <Product />;
 };
 
 export default App;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,120 @@
+import styled from 'styled-components';
+import Announcement from '../Home/Announcement';
+import { Footer } from '../Home/Footer';
+import { Navbar } from '../Home/Navbar';
+import { Newsletter } from '../Home/Newsletter';
+
+export const Product = () => {
+	return (
+		<Container>
+			<Announcement />
+			<Navbar />
+			<Wrapper>
+				<ImgContainer>
+					<Image src='https://i.ibb.co/S6qMxwr/jean.jpg' />
+				</ImgContainer>
+				<InfoContainer>
+					<Title>Denim Jumpsuit</Title>
+					<Description>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+						veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+						velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+						cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+						est laborum.
+					</Description>
+					<Price>$35</Price>
+					<FilterContainer>
+						<Filter>
+							<FilterTitle>Color</FilterTitle>
+							<FilterColor color='black' />
+							<FilterColor color='darkblue' />
+							<FilterColor color='gray' />
+						</Filter>
+						<Filter>
+							<FilterTitle>Size</FilterTitle>
+							<FilterSize>
+								<FilterSizeOption>XS</FilterSizeOption>
+								<FilterSizeOption>S</FilterSizeOption>
+								<FilterSizeOption>M</FilterSizeOption>
+								<FilterSizeOption>L</FilterSizeOption>
+								<FilterSizeOption>XL</FilterSizeOption>
+							</FilterSize>
+						</Filter>
+					</FilterContainer>
+				</InfoContainer>
+			</Wrapper>
+			<Newsletter />
+			<Footer />
+		</Container>
+	);
+};
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+	padding: 50px;
+	display: flex;
+`;
+
+const ImgContainer = styled.div`
+	flex: 1;
+`;
+
+const InfoContainer = styled.div`
+	flex: 1;
+	padding: 0px 50px;
+`;
+
+const Image = styled.img`
+	width: 100%;
+	height: 90vh;
+	object-fit: cover;
+`;
+
+const Title = styled.h1`
+	font-weight: 200;
+`;
+
+const Description = styled.p`
+	margin: 20px 0;
+`;
+
+const Price = styled.span`
+	font-weight: 100;
+	font-size: 2.5rem;
+`;
+
+const FilterContainer = styled.div`
+	width: 50%;
+	margin: 30px 0px;
+	display: flex;
+	justify-content: space-between;
+`;
+
+const Filter = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const FilterTitle = styled.span`
+	font-size: 1.1rem;
+	font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+	width: 20px;
+	height: 20px;
+	border-radius: 50%;
+	background-color: ${(props) => props.color};
+	margin: 0 5px;
+	cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+	margin-left: 10px;
+	padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
