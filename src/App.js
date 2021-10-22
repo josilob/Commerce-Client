57dfa44f15a1eb75b5367a756dbd6b624a952df0
Bojan J <bjosilo@gmail.com>
2021-10-22 00:15:24 -0400

    styled login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Home } from './components/Routes/Home';
 import { Product } from './components/Routes/Product';
 import { ProductList } from './components/Routes/ProductList';
 import { Register } from './components/Routes/Register';
+import { Login } from './components/Routes/Login';
 
 export const App = () => {
 	return (
@@ -10,6 +11,7 @@ export const App = () => {
 			<ProductList />
 			<Product /> */}
 			<Register />
+			{/* <Login /> */}
 		</>
 	);
 };
--- src/components/Routes/Login.js
+++ src/components/Routes/Login.js
@@ -1,7 +1,77 @@
 import styled from 'styled-components';
 
 export const Login = () => {
-	return <Container></Container>;
+	return (
+		<Container>
+			<Wrapper>
+				<Title>SIGN IN</Title>
+				<Form>
+					<Input placeholder='username' />
+					<Input placeholder='password' />
+					<Button>LOGIN</Button>
+					<Link>FORGOT THE PASSOWRD?</Link>
+					<Link>CREATE A NEW ACCOUNT</Link>
+				</Form>
+			</Wrapper>
+		</Container>
+	);
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background: linear-gradient(
+			rgba(255, 255, 255, 0.5),
+			rgba(255, 255, 255, 0.15)
+		),
+		url('https://images.unsplash.com/photo-1490114538077-0a7f8cb49891')
+			center/cover no-repeat;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Wrapper = styled.div`
+	width: 25%;
+	padding: 20px;
+	background-color: #fff;
+	border: 2px solid teal;
+`;
+
+const Title = styled.h1`
+	font-size: 1.5rem;
+	font-weight: 300;
+`;
+
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+`;
+
+const Input = styled.input`
+	flex: 1;
+	min-width: 40%;
+	margin: 10px 0;
+	padding: 10px;
+	font-size: 1rem;
+`;
+
+const Button = styled.button`
+	width: 40%;
+	border: none;
+	padding: 15px 20px;
+	background-color: teal;
+	flood-color: #fff;
+	margin-bottom: 10px;
+
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+const Link = styled.a`
+	margin: 5px 0;
+	font-size: 12px;
+	text-decoration: underline;
+	cursor: pointer;
+`;
