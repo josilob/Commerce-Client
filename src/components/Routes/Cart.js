96ee9c0403262bf66f7a550c6c2efa22a6d3eedb
Bojan J <bjosilo@gmail.com>
2021-10-25 09:00:26 -0400

    displayed products in cart

--- src/components/Routes/Cart.js
+++ src/components/Routes/Cart.js
@@ -1,3 +1,4 @@
+import { Add, Remove } from '@material-ui/icons';
 import styled from 'styled-components';
 import { Announcement } from '../Announcement';
 import { Footer } from '../Footer';
@@ -10,6 +11,7 @@ export const Cart = () => {
 			<Navbar />
 			<Wrapper>
 				<Title>Your Cart</Title>
+
 				<Top>
 					<TopBtn>CONTINUE SHOPPING</TopBtn>
 					<TopTexts>
@@ -18,7 +20,63 @@ export const Cart = () => {
 					</TopTexts>
 					<TopBtn type='filled'>CHECKOUT</TopBtn>
 				</Top>
-				<Bottom></Bottom>
+
+				<Bottom>
+					<Info>
+						<Product>
+							<ProductDetails>
+								<Image src='https://cdn.modesens.com/media/101365384' />
+								<Details>
+									<ProductName>
+										<b>Product:</b> Nike Raincoat
+									</ProductName>
+									<ProductID>
+										<b>ID:</b> 109257091
+									</ProductID>
+									<ProductColor color='#7A89CB'></ProductColor>
+									<ProductSize>
+										<b>Size:</b> L
+									</ProductSize>
+								</Details>
+							</ProductDetails>
+							<PriceDetails>
+								<ProductAmount>
+									<Add />
+									<Amount>2</Amount>
+									<Remove />
+								</ProductAmount>
+								<ProductPrice>$75</ProductPrice>
+							</PriceDetails>
+						</Product>
+						<Hr />
+						<Product>
+							<ProductDetails>
+								<Image src='https://i.pinimg.com/originals/99/ae/b3/99aeb39b495a89f00eada7ace83b244c.jpg' />
+								<Details>
+									<ProductName>
+										<b>Product:</b> UnderArmour Golf Pants
+									</ProductName>
+									<ProductID>
+										<b>ID:</b> 839572930
+									</ProductID>
+									<ProductColor color='#40567F'></ProductColor>
+									<ProductSize>
+										<b>Size:</b> L
+									</ProductSize>
+								</Details>
+							</ProductDetails>
+							<PriceDetails>
+								<ProductAmount>
+									<Add />
+									<Amount>1</Amount>
+									<Remove />
+								</ProductAmount>
+								<ProductPrice>$45</ProductPrice>
+							</PriceDetails>
+						</Product>
+					</Info>
+					<Summary></Summary>
+				</Bottom>
 			</Wrapper>
 			<Footer />
 		</Container>
@@ -40,6 +98,7 @@ const Top = styled.div`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+	padding: 20px;
 `;
 
 const TopBtn = styled.button`
@@ -60,4 +119,79 @@ const TopText = styled.span`
 	margin: 0 10px;
 `;
 
-const Bottom = styled.div``;
+const Bottom = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
+const Info = styled.div`
+	flex: 3;
+`;
+
+const Summary = styled.div`
+	flex: 1;
+	background-color: #000;
+`;
+
+const Hr = styled.hr`
+	background-color: #eee;
+	border: none;
+	height: 1px;
+`;
+
+const Product = styled.div`
+	display: flex;
+	justify-content: space-space-between;
+`;
+
+const ProductDetails = styled.div`
+	flex: 2;
+	display: flex;
+`;
+
+const Image = styled.img`
+	width: 200px;
+`;
+
+const Details = styled.p`
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductID = styled.span``;
+
+const ProductColor = styled.span`
+	width: 20px;
+	height: 20px;
+	border-radius: 50%;
+	background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetails = styled.div`
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
+const ProductAmount = styled.div`
+	display: flex;
+	align-items: center;
+	margin-bottom: 20px;
+`;
+
+const Amount = styled.span`
+	font-size: 1.5rem;
+	margin: 5px;
+`;
+
+const ProductPrice = styled.span`
+	font-size: 1.9rem;
+`;
