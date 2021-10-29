b5cc80691672eb2dd96ef9ee643ca0a8620ca81b
Bojan J <bjosilo@gmail.com>
2021-10-29 14:48:58 -0400

    implemented react-router with component routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from './components/Routes/Home';
 import { Product } from './components/Routes/Product';
 import { ProductList } from './components/Routes/ProductList';
@@ -7,13 +8,15 @@ import { Cart } from './components/Routes/Cart';
 
 export const App = () => {
 	return (
-		<>
-			<Home />;
-			{/* <ProductList />; */}
-			{/* <Product />; */}
-			{/* <Register />; */}
-			{/* <Login /> ; */}
-			{/* <Cart /> */}
-		</>
+		<Router>
+			<Switch>
+				<Route exact path='/' render={() => <Home />} />
+				<Route path='/products/:category' render={() => <ProductList />} />
+				<Route path='/product/:id' render={() => <Product />} />
+				<Route path='/register' render={() => <Register />} />
+				<Route path='/login' render={() => <Login />} />
+				<Route path='/cart' render={() => <Cart />} />
+			</Switch>
+		</Router>
 	);
 };
